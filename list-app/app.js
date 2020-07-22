2140080669361de760b0b7013cd8392f84b287ca
MochaRidwan <mocharidwann@gmail.com>
2020-07-22 10:45:23 +0700

    new list-app

---
+++ list-app/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mysql = require("mysql");
+
+const app = express();
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "kosmetik",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log("error connecting: " + err.stack);
+    return;
+  }
+  console.log("success coy");
+});
+
+app.get("/", (req, res) => {
+  connection.query("SELECT * FROM produk", (error, results) => {
+    console.log(results);
+    res.render("hello.ejs", { item: results[0] });
+  });
+});
+
+app.listen(3000);
